f58237cc8f354edc2df813cbe845a93ad97522c1
Ari Maulana <m.ari.maulana93@gmail.com>
2025-12-30 11:32:31 +0700

    vercel express final config

--- src/app.js
+++ src/app.js
@@ -1,14 +1,10 @@
 const express = require('express');
-
 const app = express();
+
 app.use(express.json());
 
-const healthRoute = require('./routes/health');
-
-app.use('/health', healthRoute);
-
 app.get('/', (req, res) => {
-  res.json({ message: 'Express + Neon + Vercel OK' });
+  res.json({ message: 'API LIVE' });
 });
 
 module.exports = app;
